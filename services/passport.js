08aaefc4e4836dfb73c7067579547f34f66b7538
Olushi Luqman O <olushilukmon03@gmail.com>
2018-01-01 23:32:35 +0100

    replaced promise with AsyncAwait

--- services/passport.js
+++ services/passport.js
@@ -9,33 +9,20 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-    User.findById(id)
-    .then(user => {
-        done(null, user);
-    }).catch(err => {
-        done(err, null);
-    });
+passport.deserializeUser(async (id, done) => {
+    const user = await User.findById(id);
+    return done(null, user);
 })
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientId,
     clientSecret: keys.googleSecret,
     callbackURL: '/auth/google/callback',
-    proxy: true,
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({googleId: profile.id})
-    .then(user => {
-        if (user) done(null, user);
-        else {
-            new User({googleId: profile.id}).save()
-            .then(user => done(null, user))
-            .catch(err => {
-                done(err, null);
-            })
-        }
-    })
-    .catch(err => {
-        console.log(err);
-    })
+    proxy: true
+}, async(accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({googleId: profile.id});
+    if (existingUser) 
+        return done(null, existingUser);
+    const newUser = await User({googleId: profile.id}).save();
+    return done(null, newUser);
 }));
