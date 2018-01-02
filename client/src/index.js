c967535b0ee5d7b8811753815f712bd9f1a78448
Olushi Luqman O <olushilukmon03@gmail.com>
2018-01-02 09:21:52 +0100

    created authReducer

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 
-// react components
+// components
 import App from './components/App'
 
-const store = createStore(() => [], {}, applyMiddleware());
+// reducers
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
     <Provider store={store}>
