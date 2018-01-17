fe04681b5c7ea298249d76107f373b18e85c3f2f
Olushi Luqman O <olushilukmon03@gmail.com>
2018-01-17 04:05:40 +0100

    polish checkout btn

--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -6,11 +6,15 @@ export default class Payment extends Component {
         return (
             <div>
                 <StripeCheckout
+                    name={process.env.REACT_APP_NAME}
+                    description="$5 for 5 email credits."
                     amount={500}
                     stripeKey={process.env.REACT_APP_STRIPE_KEY}
                     token={(token) => {
                     console.log(token);
-                }}/>
+                }}>
+                    <button className="btn">Add credits</button>
+                </StripeCheckout>
             </div>
         )
     }
