90da560e0119bfe256913f5d226ebf1de7b9d149
Olushi Luqman O <olushilukmon03@gmail.com>
2018-01-17 06:13:31 +0100

    hook up middleware

--- appMiddlewares/index.js
+++ appMiddlewares/index.js
@@ -1,13 +1,16 @@
+const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const {cookieSecret} = require('../config/keys');
 const passport = require('passport');
 
 module.exports = app => {
+    app.use(bodyParser.json());
+
     app.use(cookieSession({
         maxAge: 60 * 60 * 24 * 30,
         keys: [cookieSecret]
     }))
-    
+
     app.use(passport.initialize());
     app.use(passport.session());
 }
