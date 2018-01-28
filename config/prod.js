29e61a7107081a690f0c468ab6c8683bf4348c9a
Olushi Luqman O <olushilukmon03@gmail.com>
2018-01-28 19:19:11 +0100

    added sendgrid api key

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,6 @@ module.exports = {
   database: process.env.DATABASE,
   cookieSecret: process.env.COOKIE_SECRET,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SENDGRID_KEY
 }
